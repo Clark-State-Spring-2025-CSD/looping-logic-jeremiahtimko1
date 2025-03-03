270cb58b698baaafa82bdcbad5c7f85724ab81ab
jeremiahtimko1 <119469430+jeremiahtimko1@users.noreply.github.com>
2025-03-03 12:27:41 -0500

    Update fizzbuzz.py finished

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,13 @@
 #13
 #14
 #FizzBuzz
+
+for num in range(1, 26):  # Loop from 1 to 25
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
